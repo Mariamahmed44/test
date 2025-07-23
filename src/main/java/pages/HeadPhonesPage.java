30fd11975a18997403bd69a1cb134513a468efd4
Sherriif <sherifhamada299@gmail.com>
2025-07-24 02:44:26 +0300

    Click on first headphone in headphones page and navigated to the headphone details

--- src/main/java/pages/HeadPhonesPage.java
+++ src/main/java/pages/HeadPhonesPage.java
@@ -1,4 +1,33 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class HeadPhonesPage {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    //CONSTRUCTOR
+    public HeadPhonesPage(WebDriver driver){
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    //Locators
+    By BeatsStudioHeadPhone = By.xpath("//img[@id='15']");
+
+    //METHODS
+    public BeatsStudio2OverEarMatteBlackHeadPhonesDetailsPage ClickOnProduct(){
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(BeatsStudioHeadPhone));
+        element.click();
+        return new BeatsStudio2OverEarMatteBlackHeadPhonesDetailsPage(driver);
+    }
+
+
+
 }
