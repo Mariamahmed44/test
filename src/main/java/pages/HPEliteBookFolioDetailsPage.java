9861cb7125532284b79b7814757c255ea9a9d09d
Mariamahmed44 <115891752+Mariamahmed44@users.noreply.github.com>
2025-07-24 02:55:20 +0300

    Adding locators and methods for elite book PDP
    Adding locators and methods for elite book PDP

--- src/main/java/pages/HPEliteBookFolioDetailsPage.java
+++ src/main/java/pages/HPEliteBookFolioDetailsPage.java
@@ -1,4 +1,42 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 public class HPEliteBookFolioDetailsPage {
+   private WebDriver driver;
+    private WebDriverWait wait;
+
+    public HPEliteBookFolioDetailsPage (WebDriver driver){
+        this.driver=driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+    }
+
+    final private By ProductPageIdentity = By.xpath("//h2[@class='roboto-regular product_specifications ng-scope']");
+    final private By ProductAddToCart = By.xpath("//button[@name='save_to_cart']");
+    final private By ProductName = By.xpath("//h1[@class='roboto-regular screen768 ng-binding']");
+
+
+    //Methods
+    public boolean VerifyEliteBookPDP(){
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(ProductPageIdentity));
+        return wait.until(ExpectedConditions.visibilityOf(element)).isDisplayed();
+    }
+    public HPEliteBookFolioDetailsPage EliteBookClickAddToCart() {
+        driver.findElement(ProductAddToCart).click();
+
+        return null;
+    }
+      public String VerifyEliteBookName() {
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(ProductName));
+        return wait.until(ExpectedConditions.visibilityOf(element)).getText();
+
+
+    }
+  
 }
