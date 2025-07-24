c380f9ca22f330e459356cc92b1789a897994890
Mariamahmed44 <115891752+Mariamahmed44@users.noreply.github.com>
2025-07-24 18:02:42 +0300

    Update HPElitePad1000G2TabletDetailsPage.java
    Adding locators and methods for the HP elite pad G2 PDP

--- src/main/java/pages/HPElitePad1000G2TabletDetailsPage.java
+++ src/main/java/pages/HPElitePad1000G2TabletDetailsPage.java
@@ -1,4 +1,42 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 public class HPElitePad1000G2TabletDetailsPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public HPElitePad1000G2TabletDetailsPage (WebDriver driver){
+        this.driver=driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+    }
+    //locators
+    final private By ProductPageIdentity = By.xpath("//h2[@class='roboto-regular product_specifications ng-scope']");
+    final private By ProductAddToCart = By.xpath("//button[@name='save_to_cart']");
+    final private By ProductName = By.xpath("//h1[@class='roboto-regular screen768 ng-binding']");
+
+
+    //Methods
+    public boolean VerifyElitePadG2PDP(){
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(ProductPageIdentity));
+        return wait.until(ExpectedConditions.visibilityOf(element)).isDisplayed();
+    }
+    public HPElitePad1000G2TabletDetailsPage ElitePadG2ClickAddToCart() {
+        driver.findElement(ProductAddToCart).click();
+
+        return null;
+    }
+    public String VerifyElitePadG2Name() {
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(ProductName));
+        return wait.until(ExpectedConditions.visibilityOf(element)).getText();
+
+
+    }
+
 }
