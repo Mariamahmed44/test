c68f8ad6b88543455722fc2d4fdab56dab26f72a
Sherriif <sherifhamada299@gmail.com>
2025-07-24 19:23:23 +0300

    Choose 2 tablets

--- src/main/java/pages/TabletsPage.java
+++ src/main/java/pages/TabletsPage.java
@@ -1,4 +1,35 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class TabletsPage {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    //CONSTRUCTOR
+    public TabletsPage(WebDriver driver){
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    //Locators
+    By HPElitePad100 = By.xpath("//img[@id='16']");
+    By HPEliteX21011 = By.xpath("//img[@id='17']");
+
+    //METHODS
+    public HPElitePad1000G2TabletDetailsPage ClickOnTheProduct(){
+        driver.findElement(HPElitePad100).click();
+        return new HPElitePad1000G2TabletDetailsPage(driver);
+    }
+
+    public HPElite_x2_1011G1TabletDetailsPage ClickOnProduct(){
+        driver.findElement(HPEliteX21011).click();
+        return new HPElite_x2_1011G1TabletDetailsPage(driver);
+    }
+
+
 }
