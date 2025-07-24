d707a5d7a857779200782afdfdfa76c05938a21d
Mohamed Ali Ragab <MOHAMEDRAGEB2020@example.com>
2025-07-24 22:35:58 +0300

    Login Page

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.time.Duration;
 
 /**
- * Page Object class for the login functionality using
+ * Page Object class for the login functionality
  */
 public class LoginPage {
 
