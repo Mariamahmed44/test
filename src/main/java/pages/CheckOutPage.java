ba6441f03072787f28e8fdeae8f215204968b4fe
Sherriif <sherifhamada299@gmail.com>
2025-07-24 05:40:26 +0300

    Checkout page

--- src/main/java/pages/CheckOutPage.java
+++ src/main/java/pages/CheckOutPage.java
@@ -1,4 +1,19 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class CheckOutPage {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    //CONSTRUCTOR
+    public CheckOutPage(WebDriver driver){
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+
 }
