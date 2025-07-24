a2a09b8b3c83dadd50b388e5b20df467cc142b98
Sherriif <sherifhamada299@gmail.com>
2025-07-24 19:19:55 +0300

    Click edit button in cart page to edit HP Elite Pad 1000

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -1,4 +1,29 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class CartPage {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    //CONSTRUCTOR
+    public CartPage(WebDriver driver){
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    //LOCATORS
+    By EditBtn = By.xpath("//a[@class='edit ng-scope']");
+
+
+
+    //METHODS
+    public HPElitePad1000G2TabletDetailsPage ClickEdit(){
+        driver.findElement(EditBtn).click();
+        return new HPElitePad1000G2TabletDetailsPage(driver);
+    }
 }
